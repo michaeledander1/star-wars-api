9d71bdc325f83794dfe394597338f09be33b0b64
michaeledander1 <michaeledander1@gmail.com>
2018-11-29 11:05:19 +0100

    can sort by age

--- src/films/controller.ts
+++ src/films/controller.ts
@@ -23,7 +23,7 @@ export default class MainController {
       }
 
     @Get("/films/:id([0-9]+)/characters/height/:asc?")
-    async getCharacters(
+    async getCharactersByHeight(
       @Param('id') id: number,
       @Param('asc') asc: string
     ) {
@@ -38,7 +38,24 @@ export default class MainController {
             .orderBy("character.height", "DESC")
             .getMany()  
         } else throw new BadRequestError('Incorrect param passed')
+    }
 
+    @Get("/films/:id([0-9]+)/characters/age/:asc?")
+    async getCharacters(
+      @Param('id') id: number,
+      @Param('asc') asc: string
+    ) {
+        if (asc === 'young') {
+          return await Character.createQueryBuilder("character")
+            .where("character.film_id = :film_id", { film_id: id })
+            .orderBy("character.age", "ASC")
+            .getMany()
+        } else if (asc === 'old') {
+          return await Character.createQueryBuilder("character")
+            .where("character.film_id = :film_id", { film_id: id })
+            .orderBy("character.age", "DESC")
+            .getMany()  
+        } else throw new BadRequestError('Incorrect param passed')
     }
 
     @Get("/planets/:climate")
